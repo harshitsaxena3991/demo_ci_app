5d16d41d7bc45b2c1ab228cbc69ee3de3b00778e
Saxena, Harshit (US - Mumbai) <hasaxena@deloitte.com>
2017-02-14 17:21:10 +0530

    Function Push

--- app/src/main/java/com/example/hasaxena/mydemociapp/MainActivity.java
+++ app/src/main/java/com/example/hasaxena/mydemociapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.hasaxena.mydemociapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final TextView hello = (TextView) findViewById(R.id.hello);
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, hello.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
